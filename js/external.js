b9e81d3bb235120ddb3192f63534a32c565b3e6c
Yasmine Rico <ricolyasmine@gmail.com>
2022-01-31 20:06:57 -0600

    Complete exercises

---
+++ js/external.js
@@ -0,0 +1,31 @@
+console.log("Hello from external JavaScript.");
+
+// alert("Welcome to my Website!");
+
+// var favoriteColor = prompt("What's your favorite color?");
+// alert("Great, " + favoriteColor + " is my favorite color too!");
+
+    //Previous Exercise Question 1
+// var pricePerDayInDollars = 3;
+// var daysOfLittleMermaid = Number(prompt("For how many days are you renting The Little Mermaid?"));
+// var daysOfBrotherBear = Number(prompt("For how many days are you renting Brother Bear?"));
+// var daysOfHercules = Number(prompt("For how many days are you renting Hercules?"));
+// var total = (daysOfLittleMermaid + daysOfBrotherBear + daysOfHercules) * pricePerDayInDollars;
+// alert("Your total will be $" + total);
+
+    //Previous Exercise Question 2
+// var whatGooglePaysInDollarsPerHour = Number(prompt("How much does Google pay you per hour?"));
+// var hoursWorkedForGoogle = Number(prompt("How many hours did you work for Google this week?"));
+// var whatAmazonPaysInDollarsPerHour = Number(prompt("How much does Amazon pay you per hour?"));
+// var hoursWorkedForAmazon = Number(prompt("How many hours did you work for Amazon this week?"));
+// var whatFacebookPaysInDollarsPerHour = Number(prompt("How much does Facebook pay you per hour?"));
+// var hoursWorkedForFacebook = Number(prompt("How many hours did you work for Facebook this week?"));
+// var myMoneyThisWeek = (whatGooglePaysInDollarsPerHour * hoursWorkedForGoogle) + (whatAmazonPaysInDollarsPerHour * hoursWorkedForAmazon) + (whatFacebookPaysInDollarsPerHour * hoursWorkedForFacebook);
+// alert("You made " + myMoneyThisWeek + " dollars this week!");
+
+    //Previous Exercise Question 3
+var classNotFull = confirm("Is the class full?");
+var noScheduleConflict = confirm("Do you have a schedule conflict?");
+var canEnroll = (classNotFull && noScheduleConflict);
+alert("It is " + canEnroll + " that you can enroll in this class");
+
