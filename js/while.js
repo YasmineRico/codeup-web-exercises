1105386b1a9ca524bbffd855eea558440a0c3f6f
Yasmine Rico <ricolyasmine@gmail.com>
2022-02-07 13:35:52 -0600

    add second possible solution to while-loop exercise

--- js/while.js
+++ js/while.js
@@ -1,7 +1,15 @@
-var counter = 1;
-while (counter < 65536){
-	counter = counter * 2;
-	console.log(counter);
+// var counter = 1;
+// var num = 2
+// while (counter < 65536){
+// 	counter = counter * num;
+// 	console.log(counter);
+//
+// }
+//or
 
+var counter =1;
+while(counter < 65536){
+	counter = counter *2;
+	console.log(counter);
 }
 
