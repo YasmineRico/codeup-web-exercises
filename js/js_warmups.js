cf76e675acad8b9cc61af913df45f21065636242
Yasmine Rico <ricolyasmine@gmail.com>
2022-02-16 09:29:31 -0600

    complete exercise and bonus

--- js/js_warmups.js
+++ js/js_warmups.js
@@ -9,15 +9,33 @@
 // If jbomb were not an admin, the message should display...
 // ‘User jbomb is not an admin and can be reached at john@email.com’
 
-var createUserMsg= [
-	{username: 'Yasmine', email: "ricolyasmine@gmail.com", isAdmin: false},
-	{username: 'jbomb', email: 'john@email', isAdmin: true}
+// var createUserMsg= [
+// 	{username: 'Yasmine', email: "ricolyasmine@gmail.com", isAdmin: false},
+// 	{username: 'jbomb', email: 'john@email', isAdmin: true}
+//
+// ]
+//
+// createUserMsg.forEach(function(users){
+// 	console.log("User " + users.username  + ((users.isAdmin == true) ? " is an admin" : " is not an admin" ) + " and can be reached at " +users.email)
+// })
 
-]
-
-createUserMsg.forEach(function(users){
-	console.log("User " + users.username  + ((users.isAdmin == true) ? " is an admin" : " is not an admin" ) + " and can be reached at " +users.email)
-})
 
 
+function areaOrPerimeter(l, w) {
+	var areaSquare = l ** 2
+	var perimeterRectangle = (l + w) * 2
+	if (typeof l === "string" || typeof w === "string") {
+		return false;
+	}
+	if (l == w) {
+		return areaSquare
+	}
+	if (l !== w) {
+		return perimeterRectangle;
+	}
+}
 
+console.log(areaOrPerimeter(6, 10))
+console.log(areaOrPerimeter("my", "hello"))
+
+//return false if input is not a number or numeric string
